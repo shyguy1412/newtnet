07fbd02186a0f5cd55c034e209e84051dad04c07
Nils Ramstoeck <nilsramstoeck@gmail.com>
2023-03-04 11:00:46 +0000

    Added destructure function to Option

--- src/lib/util/monads/Option.ts
+++ src/lib/util/monads/Option.ts
@@ -2,17 +2,24 @@ type TransformFunction<T> = (value: T | undefined | null, ...args: any[]) => T |
 
 export class Option<T>{
 
-    private value: T | undefined | null;
+  private value: T | undefined | null;
 
-    constructor(value: T | undefined | null) {
-        this.value = value;
+  constructor(value: T | undefined | null) {
+    this.value = value;
+  }
+
+  run(transform: TransformFunction<T>, ...args: any[]) {
+    if (this.value ?? false) {
+      return this;
     }
 
-    run(transform: TransformFunction<T>, ...args: any[]) {
-        if (this.value ?? false) {
-            return this;
-        }
+    this.value = transform(this.value, ...args);
+  }
 
-        this.value = transform(this.value, ...args);
-    }
-}
+  static destructure<T extends Object>(obj: T) {
+    return Object.entries(obj)
+      .map(([key, value]) => ({ [key as keyof T]: new Option(value) }))
+      .reduce((prev, cur) => ({ ...prev, ...cur }), {}) as { [x in keyof T]: Option<typeof obj[x]> };
+  }
+
+} 
