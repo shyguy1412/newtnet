ea65b08caef1e7536478303eca1d0498a1c0ac0c
Nils Ramstoeck <nilsramstoeck@gmail.com>
2023-03-04 10:47:58 +0000

    Created LoginForm component

---
+++ src/components/LoginForm/index.tsx
@@ -0,0 +1,48 @@
+import { FormEvent } from "react";
+import styles from './LoginForm.module.css'
+
+interface LoginParams {
+  api_endpoint: string,
+  callback<T>(data: T): void
+}
+
+export function LoginForm<T>({ api_endpoint, callback }: LoginParams) {
+
+  async function submitForm(e: FormEvent<HTMLFormElement>) {
+    const form = e.currentTarget;
+    e.preventDefault();
+
+    const formData = [...(new FormData(form).entries())]
+      .map(entry => ({ [entry[0]]: entry[1] }))
+      .reduce((prev, cur) => {
+        return {
+          ...prev,
+          ...cur
+        }
+      });
+
+    const response = await fetch(api_endpoint, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
+    })
+
+    if(!response.ok){
+      //ERROR
+      return;
+    }
+
+    const data = await response.json() as T;
+    callback(data);
+  }
+
+  return <>
+    <form className={styles.login_form} method="GET" target={api_endpoint} onSubmit={submitForm}>
+      <input type="text" name="email" placeholder="E-Mail" />
+      <input type="password" name="password" placeholder="Password" />
+      <button>Login</button>
+    </form>
+  </>
+}
